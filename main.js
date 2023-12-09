472be6ca35fc91772aeb5d2fc88883847f89861a
mri rashid <mdislam0996@gmail.com>
2023-12-09 13:03:05 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+function digitalClock() {
+
+    let time=new Date()
+  
+    let hours=time.getHours()
+  
+    let minutes=time.getMinutes()
+  
+    let seconds=time.getSeconds()
+  
+    let timeFormat='AM';
+  
+    
+  
+    
+  
+    if(hours===0){
+  
+      hours=12
+  
+    }
+  
+    if(hours>12){
+  
+      hours=hours-12;
+  
+      timeFormat="PM"
+  
+    }
+  
+    
+  
+    hours=hours<10 ? '0'+hours: hours;
+  
+    minutes=minutes<10? '0'+minutes:minutes;
+  
+    seconds=seconds<10? '0'+seconds: seconds
+  
+    
+  
+    let finalTime=`${hours}: ${minutes}:${seconds}`
+  
+    
+  
+    document.querySelector("#time").innerText=finalTime
+  
+    document.querySelector("small").innerText=timeFormat
+  
+    setInterval(digitalClock,1000)
+  
+  }
+  
+  digitalClock()
+  
+  
